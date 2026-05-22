1ef02dcbbd2907c94b8eb886c3e7b2e2b5a76a49
Pedro Lima <pedro.henrique.ribeiro.109@ufrn.edu.br>
2026-05-22 18:58:17 -0300

    Projeto AF Sacolas

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer" id="contato">
+      <div>
+        <h3>AF Sacolas</h3>
+        <p>Artesanato Industrial de Luxo.</p>
+      </div>
+
+      <div>
+        <p>© 2023 AF Sacolas</p>
+      </div>
+    </footer>
+  );
+}
